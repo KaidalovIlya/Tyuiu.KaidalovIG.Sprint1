a6a73d73df33d518ab487053c268ac7cad2b3f27
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-10-14 18:37:17 +0500

    Task1

---
+++ Tyuiu.KaidalovIG.Sprint1.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KaidalovIG.Sprint1.Task1.V16.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint1.Task1.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double a = 1;
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(a, x, y);
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.KaidalovIG.Sprint1.Task1.V16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaidalovIG.Sprint1.Task1.V16.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint1.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три числа a, x, y");
+            double a, x, y;
+            a = double.Parse(Console.ReadLine());
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Кайдалов И. Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СМАРТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет следующее выражение               *");
+            Console.WriteLine("*  x * 5 * y + 2 * a;.                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" x * 5 * y + 2 * a;");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a, x, y));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
