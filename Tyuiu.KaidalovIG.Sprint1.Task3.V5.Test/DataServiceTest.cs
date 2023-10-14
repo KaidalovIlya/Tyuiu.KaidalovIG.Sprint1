235202e14f11135ab7fbb495cf10e0d532f97557
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-10-14 19:59:42 +0500

    Task3

---
+++ Tyuiu.KaidalovIG.Sprint1.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KaidalovIG.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint1.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double res = ds.VerstsToKilometers(100);
+            Assert.AreEqual(106.68, res, 0.01);
+        }
+    }
+}
---
+++ Tyuiu.KaidalovIG.Sprint1.Task3.V5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaidalovIG.Sprint1.Task3.V5.Lib;
+
+namespace Tyuiu.KaidalovIG.Sprint1.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число верст");
+            int num;
+            num = int.Parse(Console.ReadLine());
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Кайдалов И. Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  1.3 Операторы составного присваивания                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СМАРТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.,            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(num);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.VerstsToKilometers(num));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
